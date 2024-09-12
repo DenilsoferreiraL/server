1537a16f2b70e256608425e8f21f5679e5ff2919
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-09-12 19:03:35 -0300

    feat: add type GoalPerDay to define daily goal structure with id, title, and completedAt.

--- src/functions/get-week-summary.ts
+++ src/functions/get-week-summary.ts
@@ -61,6 +61,12 @@ export async function getWeekSummary() {
                 .groupBy(goalsCompletedInWeek.completedAtDate),
         );
 
+    type GoalPerDay = Record<string, {
+        id: string,
+        title: string,
+        completedAt: string
+    }[]>
+
     const result = await db.with(goalsCreatedUpToWeek, goalsCompletedInWeek, goalsCompletedByWeekDay)
         .select({
             completed: sql/*sql*/`(SELECT COUNT(*) FROM ${goalsCompletedInWeek})`.mapWith(
@@ -69,7 +75,7 @@ export async function getWeekSummary() {
             total: sql/*sql*/`(SELECT SUM(${goalsCreatedUpToWeek.desiredWeeklyFrequency}) FROM ${goalsCreatedUpToWeek})`.mapWith(
                 Number
             ),
-            goalsPerDay: sql/*sql*/`
+            goalsPerDay: sql/*sql*/<GoalPerDay>`
             JSON_OBJECT_AGG(
                  ${goalsCompletedByWeekDay.completedAtDate},
                  ${goalsCompletedByWeekDay.completions}
@@ -79,6 +85,6 @@ export async function getWeekSummary() {
         .from(goalsCompletedByWeekDay);
 
     return {
-        summary: result,
+        summary: result[0],
     };
 }
