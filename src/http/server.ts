501f8a5be9cffcc846bf7cd4183816b872ded821
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-09-11 21:20:48 -0300

    feat(goals): add function to retrieve pending weekly goals with completion counts.

--- src/http/server.ts
+++ src/http/server.ts
@@ -6,11 +6,20 @@ import {
 } from "fastify-type-provider-zod";
 import z from 'zod'
 import { createGoal } from '../functions/create-goal'
+import { getWeekPendingGoals } from '../functions/get-week-pending-goals';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+app.get('/pending-goals', async () => {
+  const pendingGoals = await getWeekPendingGoals()
+
+  return {
+    pendingGoals
+  }
+})
 
 app.post('/goals', {
   schema: {
@@ -35,5 +44,5 @@ app
   })
 
   .then(() => {
-    console.log('HTTP server running![')
+    console.log('HTTP server running!')
   })
